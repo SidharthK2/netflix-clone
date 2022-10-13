01201626509a947cf4f5ddda240dbfe7080332cf
sidharthK2 <sidharthkapoor.100@gmail.com>
2022-10-13 22:21:02 +0530

    add featured

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,31 @@
 import { InfoOutlined, PlayArrow } from "@material-ui/icons";
 import React from "react";
+import "./featured.scss";
 
-const Featured = () => {
+const Featured = ({ type }) => {
   return (
     <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
       <img
         width="100%"
         src="https://images4.alphacoders.com/133/133184.jpg"
@@ -11,14 +33,12 @@ const Featured = () => {
       />
       <div className="info">
         <img
-          src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
+          src="https://upload.wikimedia.org/wikipedia/commons/d/d0/Pulp_Fiction_Logo.svg"
           alt="img"
         />
         <span className="dec">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam debitis
-          fugit non harum vero suscipit, enim tempore repellat dolorem nobis
-          excepturi voluptas animi unde alias, quasi eveniet. Quibusdam, rem
-          nesciunt?
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos
+          laudantium rem qui odit iste. Quisquam unde nobis earum rem ipsa!
         </span>
         <div className="buttons">
           <button className="play">
