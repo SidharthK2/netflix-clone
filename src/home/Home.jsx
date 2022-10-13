b3b419c0af90247bff833f4626cf4269429fd65a
sidharthK2 <sidharthkapoor.100@gmail.com>
2022-10-13 18:32:20 +0530

    init commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,9 @@
+import { AcUnit } from "@material-ui/icons";
+import React from "react";
+import "./home.scss";
+
+const Home = () => {
+  return <div className="home"></div>;
+};
+
+export default Home;
