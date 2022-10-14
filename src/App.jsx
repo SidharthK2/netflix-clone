335e0bb990abfeff382198c971243e2c2ae61e10
sidharthK2 <sidharthkapoor.100@gmail.com>
2022-10-14 16:41:38 +0530

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./app.scss";
 import Home from "./pages/home/Home";
-import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 const App = () => {
-  return <Watch />;
+  return <Login />;
 };
 
 export default App;
